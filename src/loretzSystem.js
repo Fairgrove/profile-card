df072bea9c9548c5a14fbcd5b929f0a280f1de02
Fairgrove <fagerlundfr@gmail.com>
2022-11-03 15:36:51 +0100

    moved javasript to /src and added dynamic sizing of loretz System

--- src/loretzSystem.js
+++ src/loretzSystem.js
@@ -1,5 +1,5 @@
 const WIDTH = window.innerWidth;
-const HEIGHT = window.innerHeight+10;
+const HEIGHT = window.innerHeight;
 
 const canvas = document.getElementById("background");
 canvas.width = WIDTH;
@@ -8,7 +8,8 @@ canvas.height = HEIGHT;
 const ctx = canvas.getContext("2d");   
 ctx.fillStyle = 'rgba(33, 33, 36, 1)';
 ctx.fillRect(0,0, WIDTH, HEIGHT);
-  
+ctx.filter = 'blur(2px)';  
+
 function background(){
     ctx.fillStyle = 'rgba(33, 33, 36, 1)';
     ctx.fillRect(-WIDTH/2, -HEIGHT/2, WIDTH, HEIGHT);
@@ -17,12 +18,18 @@ function background(){
 function setup(){
     background();
     ctx.translate(WIDTH/2, HEIGHT/2);
+    // corners are:
+    // -WIDTH/2, -HEIGHT/2
+    // -WIDTH/2, HEIGHT/2
+    // WIDTH/2, -HEIGHT/2
+    // WIDTH/2, HEIGHT/2
+
     window.requestAnimationFrame(draw);
 }
--WIDTH/2, -HEIGHT/2, WIDTH, HEIGHT
-var x = getRandomArbitrary(-10.0, 10.0);
-var y = getRandomArbitrary(-10.0, 10.0);
-var z = getRandomArbitrary(-10.0, 10.0);
+
+var x = getRandomArbitrary(-10, 10);
+var y = getRandomArbitrary(-10, 10);
+var z = getRandomArbitrary(-10, 10);
 
 const sigma = 10;
 const rho = 28;
@@ -49,23 +56,30 @@ function draw() {
     if (xStack.length >= 333) {
         xStack.shift();
         yStack.shift();
+        zStack.shift();
 
         xStack.push(x);
         yStack.push(y);
+        zStack.push(z);
     } else {
         xStack.push(x);
         yStack.push(y);
+        zStack.push(z);
     }
 
     ctx.fillStyle = "red";
     ctx.font = "30px Arial";
-    ctx.fillText(xStack.length, -200, -250);
+    ctx.fillText(Number(xStack[xStack.length - 1]).toFixed(2), -WIDTH/2 + 20, HEIGHT/2);
+    ctx.fillText(Number(yStack[yStack.length - 1]).toFixed(2), -WIDTH/2 + 120, HEIGHT/2);
+    ctx.fillText(Number(zStack[zStack.length - 1]).toFixed(2), -WIDTH/2 + 220, HEIGHT/2);
 
     ctx.beginPath();
     ctx.strokeStyle = "White"
 
+
+    // x and y extremes are +-15 -- scale their values to width and height of screen
     for (let i = 0; i < xStack.length; i++) {    
-        ctx.lineTo(xStack[i]*40, yStack[i]*8);
+        ctx.lineTo(xStack[i]*(WIDTH/2)/20, yStack[i]*(HEIGHT/2)/20);
     }
     ctx.stroke();
     
