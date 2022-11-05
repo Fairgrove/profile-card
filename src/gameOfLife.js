7f10fdd36bf1b7ce4df87390ae04ab027eb069e8
Fairgrove <fagerlundfr@gmail.com>
2022-11-05 18:01:50 +0100

    game of life works bug free

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -12,30 +12,88 @@ ctx.fillRect(0,0, WIDTH, HEIGHT);
 
 function background(){
     ctx.fillStyle = 'rgba(33, 33, 36, 1)';
-    ctx.fillRect(-WIDTH/2, -HEIGHT/2, WIDTH, HEIGHT);
+    ctx.fillRect(0,0, WIDTH, HEIGHT);
 }
 
-const cellSize = 20;
-var grid = [[],[]]
+const cellSize = 5;
+const cols = Math.floor(WIDTH/cellSize);
+const rows = Math.floor(HEIGHT/cellSize);
+
+var grid = new Array(cols);
+
+for (var i = 0; i < grid.length; i++) {
+    grid[i] = new Array(rows);
+}
+
+for (let ii = 0; ii < cols; ii++) {
+    for (let jj = 0; jj < rows; jj++) {
+        grid[ii][jj] = Math.floor(Math.random()*2);
+    }
+}
+
+var next = grid; // for swap
+
 
 function setup(){
-    //find the amount of squares that fit in the height and width of the screen -- maybe do something where the cell size is dynamic as well, by defining a range which the cell can be of size
-
-    for (let ii = 0; ii < array.length; ii++) {
-        for (let jj = 0; jj < array.length; jj++) {
-            grid[ii][jj] = Math.random();
-        }
-    }
     background();
     window.requestAnimationFrame(draw);
+    
 }
 
 function draw() {
     background();
+    
+    ctx.fillStyle = "#7f7f8a";
+    // ctx.fillRect(0,0,20,20);
+    for (let ii = 0; ii < cols; ii++) {
+        for (let jj = 0; jj < rows; jj++) {
+            if (grid[ii][jj]){
+                ctx.fillRect(ii*cellSize, jj*cellSize, cellSize, cellSize);
+                
+                // rules for living cells
+                if (neighborSum(ii,jj) == 2 || neighborSum(ii,jj) == 3){
+                    next[ii][jj] = 1;
+                } else {
+                    next[ii][jj] = 0;
+                }
+            }
+            else {
+                // rules for dead cells
+                if (neighborSum(ii,jj) == 3){
+                    next[ii][jj] = 1;
+                } else {
+                    next[ii][jj] = 0;
+                }
+            }
+        }
+    }
 
-
+    swapGrid(); 
 
     window.requestAnimationFrame(draw);
+}
+
+function neighborSum(x, y){
+    var sum = 0;
+
+    for (let ii = -1; ii < 2; ii++) {
+        for (let jj = -1; jj < 2; jj++) {
+
+            var row = (x+ii+rows) % rows
+            var col = (y+jj+cols) % cols
+
+            sum += grid[row][col]
+        }
+    }
+
+    // subtract the given cell from the sum
+    return sum - grid[x][y]
+}
+
+function swapGrid(){
+    var temp = grid;
+    grid = next;
+    next = temp;
 }
 
 setup();
